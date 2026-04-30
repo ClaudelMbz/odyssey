40694cba44f2dc303fcbe0371f7c928d61ab1f4a
Claudel M. <146814539+ClaudelMbz@users.noreply.github.com>
2026-04-30 09:55:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ function MobileNav() {
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/odyssey">
       <AuthProvider>
         <div className="min-h-screen bg-slate-50 font-sans text-slate-900">
           <Sidebar />
